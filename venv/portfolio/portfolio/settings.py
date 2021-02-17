2363edc4a94bdad33c867c67a6e4f59bff2de99c
poiana <poianakim@gmail.com>
2021-02-17 16:39:17 +0100

    changed directory of Procfile

--- venv/portfolio/portfolio/settings.py
+++ venv/portfolio/portfolio/settings.py
@@ -47,7 +47,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'portfolio.apps.PortfolioConfig',
+
+    'portfolio',
     # 'portfolio',
     'about',
     'projects',
