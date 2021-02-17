bc958c1fcd00dd5086115825b245413053834dca
poiana <poianakim@gmail.com>
2021-02-17 10:58:25 +0100

    edited Procfile

--- venv/portfolio_1/pages/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-
-
-def home_view(request):
-    return render(request, 'home.html', {})
-
-
-def contact_view(request):
-    return render(request, 'contact.html', {})
